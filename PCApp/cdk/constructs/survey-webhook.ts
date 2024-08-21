b6d55ea49b7cafb3b53901b0977980b8d43aa9b8
Brian Mills <brian@millsit.com>
2024-08-22 07:21:04 +1000

    Working api gateway to sqs queue integration

--- PCApp/cdk/constructs/survey-webhook.ts
+++ PCApp/cdk/constructs/survey-webhook.ts
@@ -3,6 +3,7 @@ import { Construct } from "constructs";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as ApiGateway from "aws-cdk-lib/aws-apigateway";
 import { Effect, Policy, PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 //import { aws_sqs as sqs } from "aws-cdk-lib";
 
 export class SurveyWebhook extends Construct {
@@ -25,33 +26,29 @@ export class SurveyWebhook extends Construct {
       },
     });
 
-    // webhook
-    const webhookApi = new ApiGateway.RestApi(this, "PcAppSurveyWebhookApi", {
-      restApiName: "PCApp Webhook API",
-      description: "This API receives survey results from the PCApp survey form",
-    });
-
     // Create policy to allow api gateway to send message to sqs
     const credentialsRole = new Role(this, "webhook-api-role", {
       assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
     });
-    const policy = new Policy(this, "webhook-api-send-message-policy", {
-      statements: [
-        new PolicyStatement({
-          actions: ["sqs:SendMessage"],
-          effect: Effect.ALLOW,
-          resources: [surveyWebhookQueue.queueArn],
-        }),
-      ],
-    });
-    credentialsRole.attachInlinePolicy(policy);
+    // const policy = new Policy(this, "webhook-api-send-message-policy", {
+    //   statements: [
+    //     new PolicyStatement({
+    //       actions: ["sqs:SendMessage"],
+    //       effect: Effect.ALLOW,
+    //       resources: [surveyWebhookQueue.queueArn],
+    //     }),
+    //   ],
+    // });
+    // credentialsRole.attachInlinePolicy(policy);
+    surveyWebhookQueue.grantSendMessages(credentialsRole);
 
     const webhookIntegration = new ApiGateway.AwsIntegration({
       service: "sqs",
-      path: surveyWebhookQueue.queueUrl,
+      path: `${process.env.CDK_DEFAULT_ACCOUNT}/${surveyWebhookQueue.queueName}`,
+      //path: surveyWebhookQueue.queueUrl,
       integrationHttpMethod: "POST",
       options: {
-        credentialsRole,
+        credentialsRole: credentialsRole,
         requestParameters: {
           "integration.request.header.Content-Type": "'application/x-www-form-urlencoded'",
         },
@@ -59,10 +56,58 @@ export class SurveyWebhook extends Construct {
         //   "application/json": `Action=SendMessage&MessageBody=$util.urlEncode($input.body)`,
         // },
         requestTemplates: { "application/json": "Action=SendMessage&MessageBody=$input.body" },
-        passthroughBehavior: ApiGateway.PassthroughBehavior.NEVER,
+        //passthroughBehavior: ApiGateway.PassthroughBehavior.NEVER,
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseTemplates: {
+              "application/json": `{"statusCode": 200, "message": "Success"}`,
+            },
+          },
+          {
+            statusCode: "400",
+            responseTemplates: {
+              "application/json": `{"statusCode": 400, "message": "Bad Request"}`,
+            },
+          },
+          {
+            statusCode: "500",
+            responseTemplates: {
+              "application/json": `{"statusCode": 500, "message": "Internal Server Error"}`,
+            },
+          },
+        ],
       },
     });
-    webhookApi.root.addResource("responslyPC2024").addMethod("POST", webhookIntegration);
+
+    // webhook API
+    const webhookApi = new ApiGateway.RestApi(this, "PcAppSurveyWebhookApi", {
+      restApiName: "PCApp Webhook API",
+      description: "This API receives survey results from the PCApp survey form",
+    });
+    // add post method
+    webhookApi.root.addResource("responslyPC2024").addMethod("POST", webhookIntegration, {
+      methodResponses: [
+        {
+          statusCode: "200",
+          // responseModels: {
+          //   "application/json": `{"statusCode": 200, "message": "Success"}`,
+          // },
+        },
+        {
+          statusCode: "400",
+          // responseModels: {
+          //   "application/json": `{"statusCode": 400, "message": "Bad Request"}`,
+          // },
+        },
+        {
+          statusCode: "500",
+          // responseModels: {
+          //   "application/json": `{"statusCode": 500, "message": "Internal Server Error"}` as IModel,
+          // },
+        },
+      ],
+    });
     //surveyWebhookQueue.grantSendMessages(webhookIntegration);
 
     // const plan = webhookApi.addUsagePlan("UsagePlan", {
@@ -74,5 +119,12 @@ export class SurveyWebhook extends Construct {
     // });
     // const key = webhookApi.addApiKey("ApiKey");
     // plan.addApiKey(key);
+
+    // lambda
+    // new NodejsFunction(this, "ProcessSurveyResults", {
+    //   entry: "/lambda/process-survey-results.ts", // accepts .js, .jsx, .cjs, .mjs, .ts, .tsx, .cts and .mts files
+    //   handler: "handler", // defaults to 'handler'
+    //   timeout: cdk.Duration.seconds(120),
+    // });
   }
 }
--- PCApp/lambda/process-survey-results.ts
+++ PCApp/lambda/process-survey-results.ts
@@ -1,6 +1,49 @@
 import { Handler } from "aws-lambda";
+import { SQSEvent, Context, SQSHandler, SQSRecord } from "aws-lambda";
+const nodemailer = require("nodemailer");
 
-export const handler: Handler = async (event, context) => {
+export const handler: SQSHandler = async (event: SQSEvent, context: Context): Promise<void> => {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-  return context.logStreamName;
+  for (const message of event.Records) {
+    await processMessageAsync(message);
+  }
+  console.info("done");
 };
+
+async function processMessageAsync(message: SQSRecord): Promise<any> {
+  try {
+    console.log(`Processing message ${message.body}`);
+    await send365Email("admin@peoplecount.au", "brian@millsit.com", "Subject", `<p><i>Hello World</i></p><p>${message.body}</p>`, `Hello World \n\n${message.body}`);
+    await Promise.resolve(1); //Placeholder for actual async work
+  } catch (err) {
+    console.error("An error occurred");
+    throw err;
+  }
+}
+
+// Set this from config or environment variable.
+const PASSWORD = "hltvvhfqsnvfxfxf";
+
+async function send365Email(from: string, to: string, subject: string, html: string, text: string) {
+  try {
+    const transportOptions = {
+      host: "smtp-mail.outlook.com", // "smtp.office365.com",
+      port: "587",
+      auth: { user: from, pass: PASSWORD },
+      secureConnection: true,
+      tls: { ciphers: "SSLv3" },
+    };
+    const mailTransport = nodemailer.createTransport(transportOptions);
+
+    await mailTransport.sendMail({
+      from,
+      to,
+      replyTo: from,
+      subject,
+      html,
+      text,
+    });
+  } catch (err) {
+    console.error(`send365Email: An error occurred:`, err);
+  }
+}
